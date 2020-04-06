2e3e0404a0422b58cf8845cf53026e0fc80c5c5e
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-04-06 14:47:41 -0300

    DemoStringBuilderAula121

---
+++ DemoStringBuilderAula121/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DemoStringBuilderAula121.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
